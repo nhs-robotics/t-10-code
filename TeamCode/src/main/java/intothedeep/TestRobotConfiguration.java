f89b058d3618a74dd9abcb183ead1aff53156438
Arlan Zbarsky <arlan.zbarsky@gmail.com>
2024-10-01 19:53:55 -0400

    Created config files for the (presumably forthcoming) testbot and Into The Deep copetition robot

---
+++ TeamCode/src/main/java/intothedeep/TestRobotConfiguration.java
@@ -0,0 +1,99 @@
+package intothedeep;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import t10.bootstrap.AbstractRobotConfiguration;
+import t10.bootstrap.Hardware;
+import t10.novel.NovelEncoder;
+import t10.novel.NovelMotor;
+import t10.novel.mecanum.MecanumDriver;
+import t10.novel.odometry.NovelOdometry;
+import t10.novel.odometry.OdometryCoefficientSet;
+import t10.vision.Webcam;
+
+public class TestRobotConfiguration extends AbstractRobotConfiguration {
+
+    // Wheels
+    @Hardware(
+            name = "FL",
+            diameterIn = Constants.Robot.ACTUAL_DIAMETER_IN,
+            ticksPerRevolution = Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT,
+            zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+    )
+    public NovelMotor fl;
+
+    @Hardware(
+            name = "FR",
+            diameterIn = Constants.Robot.ACTUAL_DIAMETER_IN,
+            ticksPerRevolution = Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT,
+            zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+    )
+    public NovelMotor fr;
+
+    @Hardware(
+            name = "BL",
+            diameterIn = Constants.Robot.ACTUAL_DIAMETER_IN,
+            ticksPerRevolution = Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT,
+            zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+    )
+    public NovelMotor bl;
+
+    @Hardware(
+            name = "BR",
+            diameterIn = Constants.Robot.ACTUAL_DIAMETER_IN,
+            ticksPerRevolution = Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT,
+            zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+    )
+    public NovelMotor br;
+
+    @Hardware(name = "imu")
+    public IMU imu;
+
+    @Hardware(name = "odometryPerpendicular")
+    public NovelMotor odometryPerpendicular;
+
+    // NOTE: SpinningIntake has the right odometer encoder
+    @Hardware(name = "odometryRight")
+    public NovelMotor odometryRight;
+
+    // NOTE: Roller has the left odometer encoder
+    @Hardware(name = "odometryLeft")
+    public NovelMotor odometryLeft;
+
+    public TestRobotConfiguration(HardwareMap hardwareMap) {
+        super(hardwareMap);
+
+        this.imu.initialize(
+                new IMU.Parameters(new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                ))
+        );
+    }
+
+    @Override
+    public MecanumDriver createMecanumDriver() {
+        return new MecanumDriver(
+                this.fl,
+                this.fr,
+                this.bl,
+                this.br,
+                this.imu,
+                Constants.Coefficients.PRODUCTION_COEFFICIENTS
+        );
+    }
+
+    public NovelOdometry createOdometry() {
+        return new NovelOdometry(
+                new OdometryCoefficientSet(1, 1, 1),
+                new NovelEncoder(this.odometryRight.motor, Constants.Odometry.ODOMETRY_WHEEL_DIAMETER_IN, Constants.Odometry.TICKS_PER_ODOMETRY_REVOLUTION),
+                new NovelEncoder(this.odometryLeft.motor, Constants.Odometry.ODOMETRY_WHEEL_DIAMETER_IN, Constants.Odometry.TICKS_PER_ODOMETRY_REVOLUTION),
+                new NovelEncoder(this.odometryPerpendicular.motor, Constants.Odometry.ODOMETRY_WHEEL_DIAMETER_IN, Constants.Odometry.TICKS_PER_ODOMETRY_REVOLUTION),
+                Constants.Odometry.ODOMETRY_LATERAL_WHEEL_DISTANCE,
+                Constants.Odometry.ODOMETRY_PERPENDICULAR_WHEEL_OFFSET
+        );
+    }
+}
