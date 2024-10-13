5a7867a4930ea96738093eefb49af71bacd48295
Arlan Zbarsky <arlan.zbarsky@gmail.com>
2024-10-13 17:40:44 -0400

    Fixed ODOMETRY_LATERAL_WHEEL_DISTANCE - it was double what it should have been

--- TeamCode/src/main/java/intothedeep/Constants.java
+++ TeamCode/src/main/java/intothedeep/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
     }
 
     public static class Odometry {
-        public static final double ODOMETRY_LATERAL_WHEEL_DISTANCE = 12;
+        public static final double ODOMETRY_LATERAL_WHEEL_DISTANCE = 6;
 
         /**
          * Distance (in) from the perpendicular odometry wheel to the center of rotation of the robot.
