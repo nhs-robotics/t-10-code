cc0454d57aec2ffe1282e6e1bf03919810da3819
EJS70 <ejs70@students.needham.k12.ma.us>
2024-10-22 18:46:34 -0400

    Test message

--- TeamCode/src/main/java/intothedeep/auto/ExampleEasyAuto.java
+++ TeamCode/src/main/java/intothedeep/auto/ExampleEasyAuto.java
@@ -6,12 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 public class ExampleEasyAuto extends EasyAuto {
     @Override
     public void run() {
-        rotationalMovement(90,2);
-        sleep(1);
-        rotationalMovement(-90,2);
-        sleep(2);
-        rotationalMovement(45,2);
-        sleep(1);
-        rotationalMovement(-45,2);
+        horizontalMovement(20, 3);
     }
 }
