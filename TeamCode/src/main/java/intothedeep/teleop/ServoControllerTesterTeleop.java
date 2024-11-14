791b5469254bebe0efc69e1c0fdeda4500f7ad76
liam <ltb271@students.needham.k12.ma.us>
2024-11-14 14:13:10 -0500

    Initial commit

---
+++ TeamCode/src/main/java/intothedeep/teleop/ServoControllerTesterTeleop.java
@@ -0,0 +1,43 @@
+package intothedeep.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import intothedeep.Constants;
+import t10.bootstrap.TeleOpOpMode;
+import t10.gamepad.GController;
+import t10.novel.NovelPositionalDCMotor;
+
+@TeleOp(name = "DCMotor Servo Teleop")
+public class ServoControllerTesterTeleop extends TeleOpOpMode {
+    private GController gamepadController;
+    public List<NovelPositionalDCMotor> servos = new ArrayList<>();
+    private Telemetry.Item servoTelemetry;
+
+    @Override
+    public void initialize() {
+        // TEMP SPEED OF 0.1 FOR TESTING BECAUSE THINGS BROKE
+        servos.add(new NovelPositionalDCMotor(hardwareMap.get(DcMotorEx.class, "linearSlide1"), Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT, 0, 1, 0.1, 1));
+        servos.add(new NovelPositionalDCMotor(hardwareMap.get(DcMotorEx.class, "linearSlide2"), Constants.TickCounts.MOVEMENT_MOTOR_TICK_COUNT, 0, 1, 0.1, -1));
+
+        this.gamepadController = new GController(this.gamepad1)
+                .x.onPress(() -> {
+                    servos.get(0).setPosition(1);
+                    servos.get(1).setPosition(1);
+                }).ok();
+        this.servoTelemetry = this.telemetry.addData("Servos positions ", servos.get(0).getPosition() + ", " + servos.get(1).getPosition());
+    }
+
+    @Override
+    public void loop() {
+        this.gamepadController.update();
+        this.servos.get(0).update();
+        this.servos.get(1).update();
+        this.servoTelemetry.setValue("Servos positions ", servos.get(0).getPosition() + ", " + servos.get(1).getPosition());
+    }
+}
---
+++ TeamCode/src/main/java/t10/novel/NovelPositionalDCMotor.java
@@ -0,0 +1,55 @@
+package t10.novel;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import java.security.InvalidParameterException;
+
+public class NovelPositionalDCMotor {
+
+    private final DcMotorEx motor;
+    private final double ticksPerRevolution;
+    private final double startPosition;
+    private final double endPosition;
+    private final double speed;
+    private double currentPosition;
+    private int coefficient;
+    private double targetPosition;
+    private double startTicksPosition;
+    private double positionBeforeMovement;
+    public NovelPositionalDCMotor(DcMotorEx motor, double ticksPerRevolution, double startPosition, double endPosition, double speed, int coefficient) {
+        if (startPosition >= endPosition) {
+            throw new InvalidParameterException("startPosition must be less than endPosition");
+        }
+
+        this.motor = motor;
+        this.ticksPerRevolution = ticksPerRevolution;
+        this.startPosition = startPosition;
+        this.currentPosition = startPosition;
+        this.endPosition = endPosition;
+        this.coefficient = coefficient;
+        this.speed = speed;
+    }
+
+    public void setPosition(double position) {
+        if (position < startPosition || position > endPosition) {
+            throw new InvalidParameterException("Position is not within bounds [" + startPosition + "," + endPosition + "]");
+        }
+
+        this.targetPosition = position;
+        this.startTicksPosition = motor.getCurrentPosition();
+        this.positionBeforeMovement = currentPosition;
+    }
+
+    public void update() {
+        double revolutions = targetPosition - currentPosition;
+        double ticks = revolutions * ticksPerRevolution;
+
+        motor.setVelocity(ticks * coefficient * speed);
+
+        currentPosition = ((motor.getCurrentPosition() - startTicksPosition) / ticksPerRevolution) + positionBeforeMovement;
+    }
+
+    public double getPosition() {
+        return currentPosition;
+    }
+}
