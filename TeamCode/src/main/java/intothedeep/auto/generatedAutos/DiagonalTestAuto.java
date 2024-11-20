6e46357578f839cda4256e11bc214f8349158f3b
EJS70 <ejs70@students.needham.k12.ma.us>
2024-11-19 19:01:03 -0500

    Created GeneratedAutos folder with generated auto opmodes

---
+++ TeamCode/src/main/java/intothedeep/auto/generatedAutos/DiagonalTestAuto.java
@@ -0,0 +1,17 @@
+// Auto-Generated Autonomous Opmode DiagonalTestAuto Created by AutoBuilder TeleOp
+package intothedeep.auto.generatedAutos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import intothedeep.auto.EasyAuto;
+
+@Autonomous(name = "DiagonalTestAuto")
+public class DiagonalTestAuto extends EasyAuto {
+	@Override
+	public void run() {
+		diagonalMovement(14.60885670162091, 16.971634592039475);
+		diagonalMovement(13.575551000247476, -20.714318097201208);
+		diagonalMovement(-13.523516637043734, 20.774933560938372);
+		diagonalMovement(-15.972128436709943, -17.950518099589303);
+	}
+}
