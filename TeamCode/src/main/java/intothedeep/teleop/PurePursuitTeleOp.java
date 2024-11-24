d0bb7d9d4816657766476c0026cf4fd47e25c70e
EJS70 <ejs70@students.needham.k12.ma.us>
2024-11-24 15:31:21 -0500

    Added check for null target path point

--- TeamCode/src/main/java/intothedeep/teleop/PurePursuitTeleOp.java
+++ TeamCode/src/main/java/intothedeep/teleop/PurePursuitTeleOp.java
@@ -9,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import intothedeep.IntoTheDeepRobotConfiguration;
 import intothedeep.KevinRobotConfiguration;
+import t10.bootstrap.AbstractRobotConfiguration;
 import t10.bootstrap.TeleOpOpMode;
 import t10.gamepad.GController;
 import t10.geometry.Pose;
@@ -23,11 +24,12 @@ import t10.motion.path.PurePursuitPathFollower;
 public class PurePursuitTeleOp extends TeleOpOpMode {
     private MecanumDriver driver;
     private GController gamepadController;
-    private IntoTheDeepRobotConfiguration c;
+    private AbstractRobotConfiguration c;
     private OdometryLocalizer odometry;
     private Telemetry.Item x;
     private Telemetry.Item y;
     private Telemetry.Item r;
+    private Telemetry.Item movementPower;
     private Telemetry.Item vert, hor;
     private PurePursuitPathFollower humanPlayerPathFollower;
     private PurePursuitPathFollower specimenRungPathFollower;
@@ -50,6 +52,7 @@ public class PurePursuitTeleOp extends TeleOpOpMode {
         this.x = this.telemetry.addData("x_novel: ", "0");
         this.y = this.telemetry.addData("y_novel: ", "0");
         this.r = this.telemetry.addData("r_novel: ", "0");
+        this.movementPower = this.telemetry.addData("movement power: ", "0");
         this.vert = this.telemetry.addData("vert: ", "0");
         this.hor = this.telemetry.addData("hor: ", "0");
         this.c = new IntoTheDeepRobotConfiguration(this.hardwareMap);
@@ -59,9 +62,9 @@ public class PurePursuitTeleOp extends TeleOpOpMode {
                 .leftBumper.initialToggleState(true).ok();  // micro-movement
         this.atl = new AprilTagLocalizer(Constants.Webcam.C270_FOCAL_LENGTH_X, Constants.Webcam.C270_FOCAL_LENGTH_Y, Constants.Webcam.C270_PRINCIPAL_POINT_X, Constants.Webcam.C270_PRINCIPAL_POINT_Y);
         this.localizer = new Localizer(
-                atl,
+                null, // TODO: Switch to this.atl when Camera is calibrated.
                 this.odometry,
-                new Pose(0, 48, 0, AngleUnit.RADIANS)
+                new Pose(0, 0, 0, AngleUnit.RADIANS) //TODO: Change x back to 48
         );
 
         // Initialize Path Followers
@@ -123,26 +126,32 @@ public class PurePursuitTeleOp extends TeleOpOpMode {
         this.pose = this.localizer.getFieldCentricPose();
 
         if (!this.gamepadController.rightBumper.isPressed() && !this.isStopRequested()) {
-            if (this.gamepadController.x.isPressed()) {
-                humanPlayerPathFollower.follow(this.driver);
-            } else if (this.gamepadController.y.isPressed()) {
-                specimenRungPathFollower.follow(this.driver);
-            } else if (this.gamepadController.a.isPressed()) {
-                specimenBasketPathFollower.follow(this.driver);
-            } else if (this.gamepadController.b.isPressed()) {
-                hangingBarPathFollower.follow(this.driver);
+            try {
+                if (this.gamepadController.x.isPressed()) {
+                    humanPlayerPathFollower.follow(this.driver);
+                } else if (this.gamepadController.y.isPressed()) {
+                    specimenRungPathFollower.follow(this.driver);
+                } else if (this.gamepadController.a.isPressed()) {
+                    specimenBasketPathFollower.follow(this.driver);
+                } else if (this.gamepadController.b.isPressed()) {
+                    hangingBarPathFollower.follow(this.driver);
+                } else {
+                    this.microMovement = this.gamepadController.leftBumper.isToggled() ? 1 : 0;
+                    this.gamepadController.update();
+                    this.driver.useGamepad(this.gamepad1, this.gamepadController.leftBumper.isToggled() ? 0.25 : 1);
+                }
+            } catch (IllegalStateException e) {
+                System.out.println("Too far from lookahead path.");
             }
         }
 
-        this.microMovement = this.gamepadController.leftBumper.isToggled() ? 1 : 0;
 
         this.x.setValue(this.odometry.getFieldCentricPose().getX());
         this.y.setValue(this.odometry.getFieldCentricPose().getY());
         this.r.setValue(this.odometry.getFieldCentricPose().getHeading(AngleUnit.DEGREES));
         this.vert.setValue(this.odometry.getRobotCentricVelocity(-1,0));
         this.hor.setValue(this.odometry.getRobotCentricVelocity(0,1));
-        this.gamepadController.update();
-        this.driver.useGamepad(this.gamepad1, this.gamepadController.x.isToggled() ? 4 : 1);
+        this.movementPower.setValue(this.gamepadController.leftBumper.isToggled() ? 0.25 : 1);
         this.telemetry.update();
         this.odometry.update();
     }
