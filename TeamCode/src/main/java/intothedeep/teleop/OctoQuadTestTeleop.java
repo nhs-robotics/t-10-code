40b7d5f07919e7c1fce772a0e79affd647b2352d
Arlan Zbarsky <arlan.zbarsky@gmail.com>
2024-12-12 17:02:24 -0500

    One more import issue

--- TeamCode/src/main/java/intothedeep/teleop/OctoQuadTestTeleop.java
+++ TeamCode/src/main/java/intothedeep/teleop/OctoQuadTestTeleop.java
@@ -15,7 +15,7 @@ import t10.bootstrap.Hardware;
 import t10.bootstrap.TeleOpOpMode;
 import t10.localizer.odometry.OdometryLocalizer;
 import t10.motion.NovelMotor;
-import t10.novel.OdometryEncoder;
+import t10.motion.OdometryEncoder;
 import t10.motion.mecanum.MecanumDriver;
 
 @TeleOp(name = "OctoQuadTestTeleop")
