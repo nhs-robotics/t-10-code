49c5baceb71438c6e92d56e4170c058981b924b6
Liam Bridgers <ltb271@students.needham.k12.ma.us>
2025-03-05 18:16:56 -0500

    Add telemetry, precision increments to ServoPositioner

--- TeamCode/src/main/java/intothedeep/teleop/ServoPositioner.java
+++ TeamCode/src/main/java/intothedeep/teleop/ServoPositioner.java
@@ -2,6 +2,7 @@ package intothedeep.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import intothedeep.SnowballConfig;
 
 import t10.bootstrap.BootstrappedOpMode;
@@ -16,13 +17,18 @@ public class ServoPositioner extends BootstrappedOpMode {
 	@Metric
 	public double pos = 0;
 
+	Telemetry.Item pos_t;
+
 	@Override
 	public void init() {
 		super.init();
 		this.config = new SnowballConfig(this.hardwareMap);
 		this.g = new GController(this.gamepad1)
 				.dpadUp.onPress(() -> pos += 0.05).ok()
-				.dpadDown.onPress(() -> pos -= 0.05).ok();
+				.dpadDown.onPress(() -> pos -= 0.05).ok()
+				.rightBumper.onPress(() -> pos += 0.01).ok()
+				.leftBumper.onPress(() -> pos -= 0.01).ok();
+		pos_t = this.telemetry.addData("Position: ", 0);
 	}
 
 	@Override
@@ -30,5 +36,6 @@ public class ServoPositioner extends BootstrappedOpMode {
 		super.loop();
 		this.g.loop();
 		this.config.clawRotate.setPosition(pos);
+		pos_t.setValue(pos);
 	}
 }
